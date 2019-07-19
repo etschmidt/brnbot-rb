dbde2249f78a5f9178aba4ad23eed1be9770a57e
Ethan <ethan.t.schmidt@gmail.com>
2019-07-19 13:20:02 -0400

    evs

--- favorite.rb
+++ favorite.rb
@@ -2,14 +2,14 @@ require 'rubygems'
 require 'twitter'
 
 client = Twitter::REST::Client.new do |config|
-	config.consumer_key        = ENV["CONSUMER_KEY"]
-	config.consumer_secret     = ENV["CONSUMER_SECRET"]
-	config.access_token        = ENV["ACCESS_TOKEN"]
-	config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+	config.consumer_key          = ENV["consumer_key"]
+	config.consumer_secret       = ENV["consumer_secret"]
+	config.access_token          = ENV["access_token"]
+	config.access_token_secret   = ENV["access_token_secret"]
 end
 
 @topic = ["satire", "business news", "comedy", "satire"].sample
 
-client.search(@topic).take(7).each do |tweet|
+client.search(@topic).take(5).each do |tweet|
 	client.favorite(tweet)
 end
